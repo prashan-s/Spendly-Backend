3f7c15c352e75fc6e8bcfe5f4dc4258680623ff3
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-02-22 19:15:54 +0530

    Add User

---
+++ src/main/kotlin/com/spendly/backend/entity/User.kt
@@ -0,0 +1,48 @@
+package com.spendly.backend.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import com.spendly.backend.types.Role
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+@Document(collection = "users")
+data class User(
+    @Id
+    val id: String? = null,
+    val username: String,
+    val password: String,
+    val email: String,
+    val role: Role,
+    val isEmailVerified: Boolean
+): UserDetails {
+
+    override fun getAuthorities(): Collection<GrantedAuthority> {
+        return listOf(SimpleGrantedAuthority(role.name))
+    }
+
+    override fun getPassword(): String {
+        return password
+    }
+
+    override fun getUsername(): String {
+        return email
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+}

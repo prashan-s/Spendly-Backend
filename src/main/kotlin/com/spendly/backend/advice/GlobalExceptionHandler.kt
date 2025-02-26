61ff455404ec91b777cab9b3e2bd338f453b866e
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-02-26 16:33:32 +0530

    Add Exception Handling

---
+++ src/main/kotlin/com/spendly/backend/advice/GlobalExceptionHandler.kt
@@ -0,0 +1,34 @@
+package com.spendly.backend.advice
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.dao.DataAccessException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.net.ConnectException
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataAccessException::class)
+    fun handleDatabaseException(ex: DataAccessException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(HttpStatus.SERVICE_UNAVAILABLE)
+            .body("<h1>SERVICE_UNAVAILABLE</h1><br>Database connection issue. Please try again later.")
+    }
+
+    @ExceptionHandler(ConnectException::class)
+    fun handleDatabaseException(ex: ConnectException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(HttpStatus.SERVICE_UNAVAILABLE)
+            .body("<h1>SERVICE_UNAVAILABLE</h1><br>Database connection issue. Please try again later.")
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleGenericException(request: HttpServletRequest, ex: Exception): ResponseEntity<String> {
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("<h1>INTERNAL SERVER ERROR</h1><br>An unexpected error occurred$ex")
+    }
+}
